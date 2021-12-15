8ed5bd2eaf3ca80cc1bf2ba75cfba3c2b87cedde
Jason Toups <me@jasontoups.com>
2021-12-14 20:09:04 -0800

    updates google auth reducer and adds actions/types file

---
+++ src/actions/types.js
@@ -0,0 +1,4 @@
+// creating these variables help with debugging string errors
+// also the variables will auto-complete in VSCode
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_OUT = 'SIGN_OUT';
